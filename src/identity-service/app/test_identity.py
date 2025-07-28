17ab65d150b2d5771fa012f35592dbb287fae5d5
szhang245 <szhang245@myseneca.ca>
2025-07-28 20:50:41 +0000

    Shuai Commit

--- src/identity-service/app/test_identity.py
+++ src/identity-service/app/test_identity.py
@@ -12,8 +12,9 @@ pytestmark = pytest.mark.asyncio
 client = TestClient(app)
 
 ## Define a base URL
-HOSTNAME = "34.44.171.179"
-BASE_URL = f"http://{HOSTNAME}/api/identity"
+# BASE_URL = "http://identity-service.default.svc.cluster.local"
+BASE_URL = "http://identity-service"
+
 @pytest.mark.asyncio
 async def test_register_and_login():
     async with AsyncClient(base_url=BASE_URL) as client:
@@ -22,12 +23,12 @@ async def test_register_and_login():
 #        assert res.status_code == 204
 
         # Register a user
-        register_resp = await client.post("/register", json={"username": "testuser3", "password": "secret"})
+        register_resp = await client.post("/register", json={"username": "testuser4", "password": "secret"})
         assert register_resp.status_code == 200
         assert "user_id" in register_resp.json()
 
         # Login with the registered user
-        login_resp = await client.post("/login", json={"username": "testuser3", "password": "secret"})
+        login_resp = await client.post("/login", json={"username": "testuser4", "password": "secret"})
         assert login_resp.status_code == 200
         data = login_resp.json()
         assert "access_token" in data
