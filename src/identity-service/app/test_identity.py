8ed23725020d2859ae4fa62218f9ace54d61a19a
szhang245 <szhang245@myseneca.ca>
2025-07-28 21:57:13 +0000

    Shuai Commit

--- src/identity-service/app/test_identity.py
+++ src/identity-service/app/test_identity.py
@@ -5,7 +5,7 @@ from fastapi.testclient import TestClient
 from unittest.mock import patch
 import warnings
 
-warnings.filterwarnings("ignore", category=DeprecationWarning, module="passlib")
+warnings.filterwarnings("ignore", category=DeprecationWarning)
 
 pytestmark = pytest.mark.asyncio
 
@@ -23,12 +23,12 @@ async def test_register_and_login():
 #        assert res.status_code == 204
 
         # Register a user
-        register_resp = await client.post("/register", json={"username": "testuser7", "password": "secret"})
+        register_resp = await client.post("/register", json={"username": "testuser8", "password": "secret"})
         assert register_resp.status_code == 200
         assert "user_id" in register_resp.json()
 
         # Login with the registered user
-        login_resp = await client.post("/login", json={"username": "testuser7", "password": "secret"})
+        login_resp = await client.post("/login", json={"username": "testuser8", "password": "secret"})
         assert login_resp.status_code == 200
         data = login_resp.json()
         assert "access_token" in data
