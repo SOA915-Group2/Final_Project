4fe8da5360fe658d7ba54155d8e520892e4a1995
szhang245 <szhang245@myseneca.ca>
2025-07-29 14:32:24 +0000

    Shuai Commit

--- test.sh
+++ test.sh
@@ -1,8 +1,8 @@
 git add .
 git commit -m "Shuai Commit"
 git push -u origin main
-kubectl delete -f k8s/deployments/test-runner.yaml
+kubectl delete -f k8s/service_test/unit/test_identity.yaml
 sleep 3
-kubectl apply -f k8s/deployments/test-runner.yaml
+kubectl apply -f k8s/service_test/unit/test_identity.yaml
 sleep 2
 kubectl logs identity-test -f
