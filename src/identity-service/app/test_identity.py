7f487aa65663cff0c99aa060f50a4db2342329ac
szhang245 <szhang245@myseneca.ca>
2025-08-01 05:25:45 +0000

    Shuai Commit

--- src/identity-service/app/test_identity.py
+++ src/identity-service/app/test_identity.py
@@ -22,12 +22,12 @@ async def test_register_and_login():
         # assert res.status_code == 204
 
         # Register a user
-        register_resp = await client.post("/register", json={"username": "testuser20", "password": "secret"})
+        register_resp = await client.post("/register", json={"username": "testuser21", "password": "secret"})
         assert register_resp.status_code == 200
         assert "user_id" in register_resp.json()
 
         # Login with the registered user
-        login_resp = await client.post("/login", json={"username": "testuser20", "password": "secret"})
+        login_resp = await client.post("/login", json={"username": "testuser21", "password": "secret"})
         assert login_resp.status_code == 200
         data = login_resp.json()
         assert "access_token" in data
